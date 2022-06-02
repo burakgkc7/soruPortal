4dcb303c4b2da85141159a40300a85127add6aab
Burak Gökçe <75858757+burakgkc7@users.noreply.github.com>
2022-06-02 21:48:14 +0300

    final

---
+++ API/vizeProje/vizeProje/ViewModel/SoruModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using vizeProje.Models;
+
+namespace vizeProje.ViewModel
+{
+    public class SoruModel
+    {
+        public int soruId { get; set; }
+
+
+        public Nullable<int> kategori { get; set; }
+        public Nullable<int> yazar { get; set; }
+
+        public string soru1 { get; set; }
+
+
+        
+
+    }
+}
